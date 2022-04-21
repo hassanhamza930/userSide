ef2634e219491158ab17c615f2bfb788805aa8d1
Hamza Hassan <hassanhamza930@gmail.com>
2022-04-22 03:51:35 +0500

    Automated Categories upgraded

--- lib/pages/home/featured/components/automatedCategories.dart
+++ lib/pages/home/featured/components/automatedCategories.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import "package:flutter/material.dart";
 import 'package:userside/pages/home/featured/components/components.dart';
 
-var categories=[
+var categories = [
   "Music Stars",
   "Film Stars",
   "Sports Stars",
@@ -25,7 +25,6 @@ var categories=[
 ];
 
 class automatedCategories extends StatefulWidget {
-
   @override
   _automatedCategoriesState createState() => _automatedCategoriesState();
 }
@@ -33,34 +32,69 @@ class automatedCategories extends StatefulWidget {
 class _automatedCategoriesState extends State<automatedCategories> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      itemCount: categories.length,
-      itemBuilder: (context,index){
-        return StreamBuilder(
-          stream: FirebaseFirestore.instance.collection("celebrities").where("interests",arrayContains:"${categories[index]}").snapshots(),
+    List<Widget> automatedCategoriesList = [];
+
+
+    var totalAds=0;
+
+    for (var i = 0; i < categories.length; i++) {
+
+
+      automatedCategoriesList.add(
+          StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("celebrities")
+              .where("interests", arrayContains: "${categories[i]}")
+              .snapshots(),
           builder: (context, snapshot) {
-            if(snapshot.hasData){
-              List<DocumentSnapshot> docs=snapshot.data.docs;
-              var celebs=[];
+            if (snapshot.hasData) {
+              List<DocumentSnapshot> docs = snapshot.data.docs;
+              var celebs = [];
               docs.forEach((element) {
-                var data=element.id;
+                var data = element.id;
                 celebs.add(data);
               });
-              if(celebs.length>0){
-                return categoryRow(context: context, categoryData: celebs, categoryName: "${categories[index]}");
-              }
-              else{
+
+              if (celebs.length > 0) {
+                totalAds=totalAds+1;
+                return categoryRow(
+                    context: context,
+                    categoryData: celebs,
+                    categoryName: "${categories[i]}");
+              } else {
                 return Container();
               }
-            }
-            else{
+            } else {
               return Container();
             }
-          }
-        );
-      },
+          })
+      );
+
+    }
+
+
+    var i=0;
+    var loop=true;
+    while(loop){
+      if(i<9){
+        print("Adding ads");
+        if(i%3==0){
+          automatedCategoriesList.insert(i, bigBanner(context: context));
+        }
+        i++;
+      }
+      else{
+        loop=false;
+      }
+    }
+
+
+
+    return ListView(
+      children: automatedCategoriesList,
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
     );
+
   }
 }
